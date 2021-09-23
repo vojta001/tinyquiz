08c6afeecde21588cb1eb67f62e99d52dc8110c2
Vojtěch Káně <vojtech.kane@gmail.com>
2021-09-24 01:25:04 +0200

    Add tooltips on homepage

--- ui/static/home.js
+++ ui/static/home.js
@@ -4,4 +4,17 @@ document.addEventListener("DOMContentLoaded", () => {
 		const code = joinForm.querySelector("input[name=\"code\"]").value;
 		joinForm.action = "/play/" + encodeURIComponent(code);
 	});
+	document.body.addEventListener("click", () => {
+		for (const help of document.querySelectorAll(".message.show")) {
+			help.classList.remove("show");
+		}
+	});
+	const helps = document.getElementsByClassName("help");
+	for (const help of helps) {
+		help.addEventListener("click", (e) => {
+			e.stopPropagation();
+			console.log(e.target);
+			e.target.parentElement.querySelector(".message").classList.toggle("show");
+		})
+	}
 });
